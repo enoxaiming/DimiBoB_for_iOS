dc134ed55141349078e8ea7aa54375fe25b3facb
HaJunHyeok <enoxaiming@naver.com>
2017-02-21 01:50:09 +0900

    initial commit

---
+++ DimiBoB/DimiBoB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DimiBoB
+//
+//  Created by 하준혁 on 21/02/2017.
+//  Copyright © 2017 HaJunHyeok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DimiBoB/DimiBoB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DimiBoB
+//
+//  Created by 하준혁 on 21/02/2017.
+//  Copyright © 2017 HaJunHyeok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
